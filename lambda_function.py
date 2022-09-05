8d916c4b46cc4f6df75347838073a690e61ad403
mykola-shorobura-omio <mykola.shorobura_ges@omio.com>
2022-09-05 19:14:02 +0200

    update lambda message

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ import json
 def lambda_handler(event, context):
     return {
       'statusCode': 200,
-      'body': json.dumps('Glory to Ukraine!')
+      'body': json.dumps('Glory to Ukraine! Glory to the heroes!')
     }
